8d5ff0a0813761e32ee15889f0fa0d33cf5b0588
sequoiahead <sequoiahead@gmail.com>
2013-02-27 11:19:10 +0100

    Project structure rearrangements
    main module deleted - it's useless
    clean-ide action is added
    eclipse-specific files are added

--- main/premake4.lua
+++
@@ -1,6 +0,0 @@
-	project "main"
-		kind "WindowedApp"
-		language "C++"
-		targetname "helmet"
-		files { "src/**.h", "src/**.cpp" }
-		links { "domain" }
--- premake4.lua
+++ premake4.lua
@@ -1,13 +1,13 @@
-DIR_TARGET = os.getcwd() .. "/target"
-DIR_BIN_DEBUG = DIR_TARGET .. "/debug/bin" 
-DIR_BIN_RELEASE = DIR_TARGET .. "/release/bin" 
-DIR_LIB_DEBUG = DIR_TARGET .. "/debug/lib" 
-DIR_LIB_RELEASE = DIR_TARGET .. "/release/lib" 
+DIR_BUILD = os.getcwd() .. "/build"
+DIR_BIN_DEBUG = DIR_BUILD .. "/debug/bin" 
+DIR_BIN_RELEASE = DIR_BUILD .. "/release/bin" 
+DIR_LIB_DEBUG = DIR_BUILD .. "/debug/lib" 
+DIR_LIB_RELEASE = DIR_BUILD .. "/release/lib" 
 
 solution "helmet"
 	configurations { "debug", "release" }
 	flags { "FatalWarnings", "ExtraWarnings" }
-	location (DIR_TARGET)
+	location (DIR_BUILD)
 	
 	configuration { "debug" }
 		defines { "_DEBUG" }
@@ -24,8 +24,19 @@ solution "helmet"
 	include "domain"
 	include "ui-cli"
 	include "ui-gtk"
-	include "main"
 
 if _ACTION == "clean" then
-	os.rmdir(DIR_TARGET)
+	os.rmdir(DIR_BUILD)
 end
+
+newaction {
+   trigger     = "clean-ide",
+   description = "Remove all IDE-specific files",
+   execute = function ()
+      local ide_resources = {".cproject", ".settings/", ".classpath", ".externalToolBuilders/", ".project"}
+		for i,res in ipairs(ide_resources) do
+			print(res) 
+			--os.rmdir(res)
+		end
+   end
+}
